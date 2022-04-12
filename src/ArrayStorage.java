23989dad7e17dc09d0ecb6d1ce2cd3bd6b04e784
asplyushchov <asplyushchov@sberbank.ru>
2022-04-12 19:23:28 +0300

    Methods implemented: save, get, delete, clear, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,64 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i <= this.size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (this.size() == 0) {
+            storage[0] = r;
+            return;
+        }
+        int i = 0;
+        while (storage[i] != null) i++;
+        storage[i] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].toString().equals(uuid)) return storage[i];
+        }
+        /**
+         * to avoid NullPointerException -  not to return "null"
+         */
+        return new Resume();
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                do {
+                    storage[i] = storage[i + 1];
+                    i++;
+                } while (storage[i] != null);
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] arrayToGet = new Resume[this.size()];
+
+        int i = 0;
+        while (storage[i] != null) {
+            arrayToGet[i] = storage[i];
+            i++;
+        }
+
+        return arrayToGet;
     }
 
     int size() {
-        return 0;
+        int counterOfResumes = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) counterOfResumes++;
+        }
+
+        return counterOfResumes;
     }
 }
