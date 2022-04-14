28b3d325c6a29d5a296ad1c7aabb60a912e9fa0d
asplyushchov <asplyushchov@sberbank.ru>
2022-04-14 20:35:59 +0300

    Issues fixed: 1. check for null deleted 2. refactoring

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,35 +6,30 @@ public class ArrayStorage {
     int size;
 
     void clear() {
-        for (int i = 0; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i <= size; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                return;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i <= size; i++) {
-            if (size != 0 && storage[i].toString().equals(uuid)) return storage[i];
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid))
+                return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i <= size; i++) {
-            if (size != 0 && storage[i].toString().equals(uuid)) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
                 for (; storage[i] != null; i++) {
                     storage[i] = storage[i + 1];
-
                 }
                 size--;
             }
@@ -45,22 +40,16 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        Resume[] arrayToGet = new Resume[this.size()];
+        Resume[] resumes = new Resume[size];
 
         for (int i = 0; storage[i] != null; i++) {
-            arrayToGet[i] = storage[i];
+            resumes[i] = storage[i];
         }
 
-        return arrayToGet;
+        return resumes;
     }
 
     int size() {
-        int counterOfResumes = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) counterOfResumes++;
-        }
-
-        return counterOfResumes;
+        return size;
     }
 }
