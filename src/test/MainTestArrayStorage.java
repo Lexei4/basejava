400cf9956eac606e46a4f1be7dde8f57a0e233b2
asplyushchov <asplyushchov@sberbank.ru>
2022-04-27 14:08:25 +0300

    UPDATE test implemented

--- src/test/MainTestArrayStorage.java
+++ src/test/MainTestArrayStorage.java
@@ -17,6 +17,9 @@ public class MainTestArrayStorage {
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
+        Resume r4 = new Resume();
+        r4.setUuid("dummyUpdate");
+
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
@@ -25,6 +28,10 @@ public class MainTestArrayStorage {
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+
+        ARRAY_STORAGE.update(r3);
+        System.out.println("dummy UPDATE result: ");
+        ARRAY_STORAGE.update(r4);
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
