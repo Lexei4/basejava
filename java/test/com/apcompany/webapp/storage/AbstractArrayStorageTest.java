9899b320c85dbd747c00b9aedbbb8c4e6d9ec0a8
out-plyushchov1-as <ASPlyushchov@sberbank.ru>
2022-05-10 18:36:58 +0300

    JUnit implemeted

---
+++ java/test/com/apcompany/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,36 @@
+package com.apcompany.webapp.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractArrayStorageTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
