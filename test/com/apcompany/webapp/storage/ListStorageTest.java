d1634851d58bb17481bace37908dcd1804ca4e5c
asplyushchov <asplyushchov@sberbank.ru>
2022-07-23 17:45:00 +0300

    saveOverflow test ignoring for non arrays

--- test/com/apcompany/webapp/storage/ListStorageTest.java
+++ test/com/apcompany/webapp/storage/ListStorageTest.java
@@ -1,10 +1,16 @@
 package com.apcompany.webapp.storage;
 
-import static org.junit.Assert.*;
+import com.apcompany.webapp.exception.StorageException;
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class ListStorageTest  extends AbstractArrayStorageTest {
     public ListStorageTest() {
         super(new ListStorage());
     }
 
+    @Ignore
+    @Test(expected = StorageException.class)
+    public void saveOverflow() {
+    }
 }
--- test/com/apcompany/webapp/storage/MapStorageTest.java
+++ test/com/apcompany/webapp/storage/MapStorageTest.java
@@ -1,7 +1,16 @@
 package com.apcompany.webapp.storage;
 
+import com.apcompany.webapp.exception.StorageException;
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class MapStorageTest extends AbstractArrayStorageTest {
     public MapStorageTest() {
         super(new MapStorage());
+    }
+
+    @Ignore
+    @Test(expected = StorageException.class)
+    public void saveOverflow() {
     }
 }
